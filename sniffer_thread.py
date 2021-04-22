6284ccd73b09b1dedeac0ea3d305251cd36f79a1
Filip Krasniqi <filip.krasniqi@polimi.it>
2021-04-22 11:01:04 +0200

    Absolute path for env

--- sniffer_thread.py
+++ sniffer_thread.py
@@ -7,13 +7,15 @@ from dotenv import get_variable
 from log_thread import LogThread
 
 
+path_env = '/home/pi/smart-directions-anchor-init/.env'
+
 class SnifferThread(LogThread):
     def __init__(self, name):
         LogThread.__init__(self, name)
         self.process = None
 
     def run(self):
-        base_path_scanner = get_variable('.env', 'BASE_PATH_SCANNER')
+        base_path_scanner = get_variable(path_env, 'BASE_PATH_SCANNER')
         command = join(base_path_scanner, "cmake-build-rasp1/ble_scanner")
         # running c++ code for sniffing
         self.process = subprocess.Popen([command])
