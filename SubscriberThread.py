ede8da8cdb02442428a0eeeb086bb7e8d9cf32e0
Filip Krasniqi <filip.krasniqi@polimi.it>
2021-06-04 12:02:58 +0200

    Removed execution of viewer. Added communication via socket for a faster visualization

--- SubscriberThread.py
+++ SubscriberThread.py
@@ -3,6 +3,8 @@ import paho.mqtt.client as mqtt
 from led_thread import LedThread
 from log_thread import LogThread
 
+import socket
+
 class SubscriberThread(LogThread):
 
     def __init__(self, mac_master, own_mac, face_id, BROKER_IP):
@@ -20,13 +22,17 @@ class SubscriberThread(LogThread):
         #print(payload)
         self.client.publish(topic,payload)
 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            self.connection = s.connect(('localhost', 6666))
+
+
     def on_connect(self, client, userdata, flags, rc):
         self.client.subscribe("#")
         self.client.message_callback_add('directions/slave/activate/#', self.show_direction)
 
     def show_direction(self, client, userdata, msg):
-        relative_msg_to_show = msg.payload.decode('utf-8')
-        LedThread("", relative_msg_to_show).start()
+        relative_msg_to_show, color, execution_time = msg.payload.decode('utf-8').split("$")
+        LedThread("", color, relative_msg_to_show, execution_time, self.connection).start()
 
     def run(self):
         self.client.loop_forever()
--- led_thread.py
+++ led_thread.py
@@ -13,33 +13,17 @@ path_env = '/home/pi/smart-directions-slave/.env'
 abs_path_arrow = "{}/../assets/".format(os.path.dirname(os.path.realpath(__file__)))
 print("PATH: {}".format(abs_path_arrow))
 PATH_ASSETS = get_variable(path_env, 'PATH_ASSETS')
-EXECUTION_TIME = get_variable(path_env, 'EXECUTION_TIME')
 
 class LedThread(LogThread):
-    def __init__(self, name, relative_message_to_show):
+    def __init__(self, name, color, direction, execution_time, connection):
         LogThread.__init__(self, name)
         self.process = None
-
-        suffix = {
-            '1': 'top',
-            '2': 'right',
-            '3': 'bottom',
-            '4': 'left',
-            '5': 'all'
-        }
-
-        arrow_to_show = suffix[relative_message_to_show]
-        self.filename = 'arrow-{}.gif'.format(arrow_to_show)
+        self.connection = connection    # socket towards c++
+        self.direction, self.color, self.execution_time = direction, color, execution_time
 
     def run(self):
-        # sudo ./led-image-viewer ../bindings/python/own_samples/arrow.gif  --led-gpio-mapping=adafruit-hat
-        base_led_image_path = get_variable(path_env, 'BASE_PATH_SCANNER')
-        #command = join(base_led_image_path, "led-image-viewer {}{}".format(abs_path_arrow, self.filename))
-        command = ['timeout', '{}s'.format(EXECUTION_TIME), join(base_led_image_path, 'led-image-viewer'), '--led-gpio-mapping=adafruit-hat', "{}/{}".format(PATH_ASSETS, self.filename)]#join(base_led_image_path, "led-image-viewer --led-gpio-mapping=adafruit-hat {}/{}".format(PATH_ASSETS, self.filename))
-        # running c++ code for sniffing
-        print("DBG: {}".format(command))
-        self.process = subprocess.Popen(command)
-        self.process.wait()
+        msg = str.encode("{}${}${}".format(self.direction, self.color, self.execution_time))
+        self.connection.sendall(msg)
 
     def kill(self):
         if self.process is not None:
