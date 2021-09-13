19278b9a76805d76c5db5d549c1720b28b15c88a
Filip Krasniqi <filip.krasniqi@polimi.it>
2021-09-13 15:43:55 +0100

    Added static definition of neighbour master

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ path_env = '/home/pi/smart-directions-slave/.env'
 face_id = get_variable(path_env, 'FACE_ID')
 BROKER_IP = get_variable(path_env, 'BROKER_IP')
 
+try:
+    NEIGHBOUR_MASTER = get_variable(path_env, 'NEIGHBOUR_MASTER')
+except:
+    NEIGHBOUR_MASTER = None
+
 if __name__ == '__main__':
     # execute command to get our own mac address (wlan)
     command = "cat /sys/class/net/wlan0/address"
@@ -30,36 +35,40 @@ if __name__ == '__main__':
     # some output
     print("Running scan to find closest master...")
     
-    # executes a scan for BLE values
-    command = "sudo timeout 20s btmon"
-    process = os.popen(command)
-    output = process.read()
-    process.close()
-    
-    results = output.split("HCI Event: ")
-    results = list(filter(lambda x: "raspberryp" in x, results))
-
-    dict_sniffed = {}   # we build a mapping [{mac, ssid, rssi}] to be localized via the fingerprinting
-    
-    for r in results:
-        mac_length = 17
-        mac = r.split("Address: ")[1][:mac_length]
-        rssi = r.split("RSSI: ")[1].split(" dBm")[0]
+    if NEIGHBOUR_MASTER is None:
+        # executes a scan for BLE values
+        command = "sudo timeout 20s btmon"
+        process = os.popen(command)
+        output = process.read()
+        process.close()
         
-        if mac in dict_sniffed:
-            dict_sniffed[mac].append(int(rssi))
-        else:
-            dict_sniffed.update({mac: []})
-    
-    dict_sniffed = {mac: mean(dict_sniffed[mac]) for mac in dict_sniffed.keys()}
+        results = output.split("HCI Event: ")
+        results = list(filter(lambda x: "raspberryp" in x, results))
 
-    max_rssi = float("-inf")
-    best_mac = None
-    for mac in dict_sniffed.keys():
-        if dict_sniffed[mac] > max_rssi:
-            max_rssi = dict_sniffed[mac]
-            best_mac = mac
-    assert best_mac is not None, "No master has been found"
+        dict_sniffed = {}   # we build a mapping [{mac, ssid, rssi}] to be localized via the fingerprinting
+        
+        for r in results:
+            mac_length = 17
+            mac = r.split("Address: ")[1][:mac_length]
+            rssi = r.split("RSSI: ")[1].split(" dBm")[0]
+            
+            if mac in dict_sniffed:
+                dict_sniffed[mac].append(int(rssi))
+            else:
+                dict_sniffed.update({mac: []})
+        
+        dict_sniffed = {mac: mean(dict_sniffed[mac]) for mac in dict_sniffed.keys()}
+
+        max_rssi = float("-inf")
+        best_mac = None
+        for mac in dict_sniffed.keys():
+            if dict_sniffed[mac] > max_rssi:
+                max_rssi = dict_sniffed[mac]
+                best_mac = mac
+        assert best_mac is not None, "No master has been found"
+    else:
+        print("Taking master from .env")
+        best_mac = NEIGHBOUR_MASTER
 
     print("INFO: Paired with: {}".format(best_mac))
     
